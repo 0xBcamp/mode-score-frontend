649cba7b53036e1b6e0f1ad8360a9f13c6c73fac
Ethan <ecala420@gmail.com>
2024-07-18 16:04:06 -0500

    Added routing when connected wallet

--- src/components/ui/ConnectButton.tsx
+++ src/components/ui/ConnectButton.tsx
@@ -1,5 +1,22 @@
+// src/components/ui/ConnectButton.tsx
 
-export default function ConnectButton() {
-    return <w3m-button />
-  }
-  
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation' // Use next/navigation instead of next/router
+import { useAccount } from 'wagmi'
+
+const ConnectButton = () => {
+  const router = useRouter()
+  const { isConnected } = useAccount()
+
+  useEffect(() => {
+    if (isConnected) {
+      router.push('#') // Redirect to the Dashboard page
+    }
+  }, [isConnected, router])
+
+  return <w3m-button />
+}
+
+export default ConnectButton
