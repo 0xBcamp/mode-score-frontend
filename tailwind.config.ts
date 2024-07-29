f9ee29085f7a6e4000135adf09110f1ff3055469
Emengkeng <blessedsonlove@gmail.com>
2024-07-29 18:38:39 +0100

    added project proposal file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/*.{png,svg,jpg}"
+    "./public/*.{png,svg,jpg}",
+    "./public/assets/*.{pptx}",
   ],
   include: ["global.d.ts"],
   theme: {
